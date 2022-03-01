7aa84f454205366e92fc1e7a69ab36e1b7823d2a
james <james@pop-os.localdomain>
2022-03-01 14:53:45 +0000

    working on unittests

--- plates/plates.py
+++ plates/plates.py
@@ -1,58 +1,91 @@
 from string import ascii_uppercase
 import re 
 import numpy as np
+from einops import rearrange
 import pandas as pd 
 
 class Plate:
     # generic
-    def __init__(self, csv, name = None, parser = None):
-        self.parser = parser # or 'ascii'
-        self.path = csv 
+    def __init__(self, 
+                 path, 
+                 fmt=384,
+                 name=None,
+                 parser='bmg-standard'):
+        assert parser in {'bmg-standard', 'bmg-ascii'}, \
+                "parser must be one of {'bmg-standard', 'bmg-ascii'}"
+        assert fmt in {96, 384}
+        self.parser = parser
+        self.path = path 
+        self.fmt = fmt 
         self.dataframe = None
         if name is None:
-            self.name = csv.split('.')[0]
+            self.name = path.split('.')[0]
         else:
             self.name = name
-
+    def __len__(self):
+        return len(self.df)
+    def __iter__(self):
+        for i in self.wells:
+            yield self.df.loc[i,:]
+    def __getitem__(self, idx):
+        pass
+    def __repr__(self):
+        return ''
     @property
-    def meta(self):
-        # dict
+    def meta(self): # returns dict
         # todo - ascii parser
         with open(self.path,'r') as f:
              m = ''.join([f.readline() for i in range(6)])
-        metadata = {'date': re.findall('Date: (\d+/\d+/\d+)', m)[0],
-                   'time': re.findall('Time: (\d+:\d+:\d+)', m)[0],
-                   'protocol':re.findall('Test name: (.+?),',m)[0],
-                   'plateID':re.findall('ID1: (.+)', m)[0]}
-        return metadata
-
+        if self.parser == 'bmg-standard':
+            return {'date': re.findall('Date: (\d+/\d+/\d+)', m)[0],
+                    'time': re.findall('Time: (\d+:\d+:\d+)', m)[0],
+                    'protocol':re.findall('Test name: (.+?),',m)[0],
+                    'plateID':re.findall('ID1: (.+)', m)[0]}
+        elif self.parser == 'bmg-ascii':
+            # this format has no metadata
+            return {}
+        else:
+            raise Exception('No parser: options are None (defau')
     @property
     def df(self):
-        if self.dataframe is None:
-            if self.parser is None:
+        if self.dataframe is None: # caching
+            if self.parser == 'bmg-standard':
                 df = self.read_standard_BMG_csv(self.path)
-            elif isinstance(self.parser, str):
-                if self.parser == 'ascii':
-                    df = self.read_BMG_ascii(self.path)
-            df.index = self.formatWells(df.index)
+                df.index = self.formatWells(df.index)
+                get_letter = lambda x : re.findall('([A-Z]+)', x)[0]
+                get_num = lambda x : int(re.findall('([0-9]+)', x)[0]) 
+                key_fn = lambda x: get_num(x) + (ascii_uppercase.index(get_letter(x))/16)
+                idx = df.index.to_list()
+                idx.sort(key=key_fn)
+                df = df.loc[idx,:]
+                self.dataframe = df 
+            elif self.parser == 'bmg-ascii':
+                with open(self.path) as f:
+                    __data = [i for i in f.read().split('\n\n') if  i != ''  ]
+                _data = np.loadtxt(self.path, delimiter=',')
+                # data.shape eg : (551, 24, 16) (wavelengths, row, col)
+                data = rearrange(_data, '(w r) c -> w c r', w=len(__data))
+                df = pd.DataFrame(rearrange(data, 'w c r -> (c r) w'))
+                if self.fmt == 96:
+                    df.index = [i for i, j in zip(\
+            [f'{k}{l}' for k in ascii_uppercase[:9] for l in range(1,13)],
+                            df.index)]
+                elif self.fmt == 384:
+                    df.index = [i for i, j in zip(\
+            [f'{k}{l}' for k in ascii_uppercase[:17] for l in range(1,25)],
+                            df.index)]
+                return df
 
-            # ensure sorting is letter:num
-            get_letter = lambda x : re.findall('([A-Z]+)', x)[0]
-            get_num = lambda x : int(re.findall('([0-9]+)', x)[0]) # risky?
-            key_fn = lambda x: get_num(x) + (ascii_uppercase.index(get_letter(x))/16)
-            idx = df.index.to_list()
-            idx.sort(key=key_fn)
-            df = df.loc[idx,:]
-            self.dataframe = df 
+            else:
+                raise Exception('Parser not recognized in plates.Plate.df')
         return self.dataframe
-
-    def formatWells(self, wells):
-        # A01 -> A1
+    @property
+    def wells(self):
+        return self.formatWells(self.df.index)
+    def formatWells(self, wells): # A01 -> A1
         fn = lambda  x : f"{re.findall('[A-Z]+', x)[0]}{int(re.findall('[0-9]+',x)[0])}"
         return [fn(i) for i in wells]
-
     def read_standard_BMG_csv(self, path):
-        # clean
         df = pd.read_csv(path, skiprows=7)
         df.index = df.iloc[:,0] # wells
         df.drop(['Unnamed: 0','Wavelength [nm]'],axis=1, inplace=True)
@@ -60,13 +93,11 @@ class Plate:
         df=df.replace('overflow', 3.5)
         df.columns = df.columns.astype(int) # wavelengths
         return df.astype(float)
-
     def read_BMG_ascii(self, path):
         # todo - robustness with plate sizes
         with open(path, 'r') as f:
             data = f.read()
-        data = data.split('\n\n') # each wavelength
-        # in layout 16 * 24 
+        data = data.split('\n\n') # each wavelength in layout 16 * 24 
         data = [i.split('\n') for i in data] # rows in each wavelength block
         data = [np.array([i.split(',') for i in j]) for j in data]
         data = np.stack([i for i in data if i.shape != (1,1)], axis=0)
@@ -76,7 +107,6 @@ class Plate:
         return pd.DataFrame([j for i in data for j in i],
                 index = wells,
                 columns = range(250,801)) # todo - check wavelengths
-
     def row(self, letter):
         idx = pd.Series(self.df.index).str.contains(letter.upper())
         return self.df.loc[idx.to_list(), :]
--- test/test.py
+++ test/test.py
@@ -1,12 +1,78 @@
+from unittest import TestCase
 import plates
 
-def testUv384m1(path):
-    plate = plates.UV384m1(path, concs = [1,2,3,5,6,7,8])
-    x = plate.blocks
-    #print(x[1].df[1])
+class testPlate(TestCase):
+    def testInitBMGStandardCsv(self):
+        p = plates.Plate('data/dm-d1,2.CSV')
+        meta = p.meta
+        df = p.df
+        fmtdWells = p.formatWells(df.index)
+        rowA = p.row('A')
+        col1 = p.col(1)
+    def testInitBMGascii(self):
+        p = plates.Plate('data/3march-echo-wt-assay-ascii.csv',
+                         parser='bmg-ascii')
+        parser = p.parser
+        path = p.path
+        dataframe = p.dataframe
+        parser = p.parser
+        meta = p.meta
+        df = p.df
+        fmtdWells = p.formatWells(df.index)
+        rowA = p.row('A')
+        col1 = p.col(1)
 
-def main():
-    testUv384m1('data/dm-d1,2.CSV')
-
-if __name__ == '__main__':
-    main()
+# class Plate:
+# class Block:
+# class BlocksConatiner:
+# class UV384(plates.Plate):
+# class UV384m1(UV384):
+# class UV384m2(UV384):
+# class UV384m3(UV384m2):
+# class UV384m4(UV384m3):
+# def r_squared(yi,yj):
+# def MichaelisMenten(x,y):
+# def __init__(self, csv, name = None, parser = None):
+# def meta(self):
+# def df(self):
+# def formatWells(self, wells):
+# def read_standard_BMG_csv(self, path):
+# def read_BMG_ascii(self, path):
+# def row(self, letter):
+# def col(self, num):
+# def rolling_var(x, ksize = 50):
+# def anomaly_detection(traces):
+# def report(block, name = None, save_path = None):
+# def subplotTraces(data, ax, concs = None, anomalies = None):
+# def subplotMichaelisMenten(x, y, ax, km, vmax, rsq):
+# def subplotText(ax, dictionary):
+# def subplotSMILES(ax, smiles, name = None):
+# def __init__(self, data, control = None, test_concs = None, ctrl_concs = None, smiles = None, smooth = False, norm410 = False):
+# def df(self):
+# def norm(self):
+# def diff(self):
+# def response(self):
+# def mm(self):
+# def __repr__(self):
+# def report(self):
+# def __init__(self, blocks):
+# def __len__(self):
+# def __getitem__(self, i):
+# def __repr__(self):
+# def plate(self):
+# def __init__(self, path, name = None, parser = None):
+# def blocknums(self):
+# def block(self, n):
+# def blocks(self):
+# def __init__(self, path, name = None, parser = None):
+# def blocknums(self):
+# def block(self, num):
+# def __init__(self, path, name = None, parser = None):
+# def blocknums(self):
+# def block(self, num):
+# def __init__(self, path, name = None, parser = None):
+# def blocknums(self):
+# def block(self, num):
+# def __init__(self, path, name = None, parser = None):
+# def block(self, num):
+# def smooth(data):
